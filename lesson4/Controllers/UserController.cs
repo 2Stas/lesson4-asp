770b307022e39e80a33f1c1764705efe7f588e03
Mr Sigma <najdyuk.stas1@gmail.com>
2026-09-03 19:20:48 +0300

    Add project files.

---
+++ lesson4/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using lesson4.Models;
+
+namespace lesson4.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(UserModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index");
+            }
+            else
+            {
+                UserModel person = new UserModel
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    PhoneNumber = model.PhoneNumber,
+                    Age = model.Age,
+                    Password = model.Password,
+                    ConfirmPassword = model.ConfirmPassword,
+                    Line = model.Line
+                };
+
+                System.IO.File.AppendAllText("user.txt", $"name: {person.Name}, email: {person.Email},phone: {person.PhoneNumber}, age: {person.Age}, password: {person.Password}, confirm password: {person.ConfirmPassword}, line: {person.Line}\n");
+                return View("Details", person);
+
+
+            }
+
+        }
+    }
+}
---
+++ lesson4/Models/UserModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+
+namespace lesson4.Models;
+
+public class UserModel
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MinLength(5)]
+    [MaxLength(30)]
+    public string? Name { get; set; }
+
+    [EmailAddress]
+    [Required(ErrorMessage = "Email is required")]
+    public string? Email { get; set; }
+
+    [Phone]
+    [RegularExpression(@"^0\d{2}-\d{3}-\d{2}-\d{2}$")]
+    [Required]
+    public string? PhoneNumber { get; set; }
+
+    [Range(1, 100)]
+    [Required]
+    public int Age { get; set; }
+
+    [Required]
+    public string? Password { get; set; }
+
+    [Compare(nameof(Password))]
+    [Required]
+    public string? ConfirmPassword { get; set; }
+
+    [Required]
+    [RegularExpression(@"^[A-Z]+$")]
+    public string? Line { get; set; }
+
+
+    //[Url]
+    //public string? Url { get; set; }
+}
