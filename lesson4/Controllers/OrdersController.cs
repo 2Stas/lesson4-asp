7c59e9540608ee2782c754e4f325dd45ac8a1514
Mr Sigma <najdyuk.stas1@gmail.com>
2026-09-03 20:44:15 +0300

    Orders task

---
+++ lesson4/Controllers/OrdersController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using lesson4.Models;
+
+namespace lesson4.Controllers
+{
+    [Route("api/orders")]
+    public class OrdersController : Controller
+    {
+        List<Order> orders = new List<Order>
+        {
+            new Order { Id = 1, Title = "Order1", Description = "Description for Order 1", Price = 10.5m, Quantity = 2 },
+            new Order { Id = 2, Title = "Order2", Description = "Description for Order 2", Price = 20.0m, Quantity = 1 },
+            new Order { Id = 3, Title = "Order3", Description = "Description for Order 3", Price = 15.75m, Quantity = 3 }
+        };
+
+        [HttpGet("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return View("Orders",orders);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var order = orders.FirstOrDefault(x => x.Id == id);
+            if(order == null)
+            {
+                return Content("Order not found");
+            }
+
+            return View("Details", order);
+        }
+
+        [HttpGet("search")]
+        public IActionResult Search(string title)
+        {
+            var result = orders.Where(x => x.Title == title).ToList();
+
+            return View("Search",result);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", order);
+            }
+            order.Id = orders.Count + 1;
+
+            orders.Append(order);
+
+            return View("Details",order);
+        }
+
+    }
+}
---
+++ lesson4/Models/OrderModel.cs
@@ -0,0 +1,22 @@
+﻿namespace lesson4.Models;
+using System.ComponentModel.DataAnnotations;
+
+public class Order
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MinLength(3)]
+    [MaxLength(50)]
+    public string? Title { get; set; }
+
+    public string? Description { get; set; }
+
+    [Range(1,10000)]
+    public decimal Price { get; set;}
+
+    [Range(1,100)]
+    public int Quantity { get; set; }
+
+    public decimal TotalPrice => Price * Quantity;
+}
